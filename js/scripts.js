ea962f9d4a2985fa93fa4a00e4a65052ff5e439d
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-02-27 11:50:52 -0800

    If applied, commit will update "type" to arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
   { name: "Bulbasaur", height: 7, type: ["Grass", "Poison"] },
-  { name: "Charmander", height: 6, type: "Fire" },
-  { name: "Squirtle", height: 5, type: "Water" },
-  { name: "Pikachu", height: 4, type: "Electric" },
+  { name: "Charmander", height: 6, type: ["Fire", "None"] },
+  { name: "Squirtle", height: 5, type: ["Water", "None"] },
+  { name: "Pikachu", height: 4, type: ["Electric", "None"] },
 ];
