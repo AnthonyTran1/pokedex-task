a9896d3fcd05b0e9b6bc694a423e4997101511a6
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-03-04 10:00:36 -0800

    If applied, commit will add entry to list and loop to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 7, types: ["Grass", "Poison"] },
-  { name: "Charmander", height: 6, types: ["Fire", "None"] },
-  { name: "Squirtle", height: 5, types: ["Water", "None"] },
-  { name: "Pikachu", height: 4, types: ["Electric", "None"] },
+  { name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"] },
+  { name: "Charmander", height: 0.6, types: ["Fire", "None"] },
+  { name: "Charizard", height: 1.7, types: ["Fire", "Flying"] },
+  { name: "Squirtle", height: 0.5, types: ["Water", "None"] },
+  { name: "Pikachu", height: 0.4, types: ["Electric", "None"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let name = pokemonList[i].name;
+  let height = pokemonList[i].height;
+  document.write(name + " (height: " + height + ")");
+  if (height >= 1.0) {
+    document.write(" - Wow, that's big! <br>");
+  } else {
+    document.write("<br>");
+  }
+}
