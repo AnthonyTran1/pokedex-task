b7ef08533cff778963e022cf067b54d4eb0c1883
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-03-14 06:40:45 -0700

    If applied, commit will remove tests

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     //check if pokemon is of correct type of data
     if (
+      typeof pokemon === "object" &&
       typeof pokemon.name === "string" &&
       typeof pokemon.height === "number" &&
       typeof pokemon.types[0] === "string" &&
@@ -57,49 +58,3 @@ function getPokemonDescription(pokemonObject) {
 //execute forEach on the PokemonList from IIFE
 pokemonRepository.getAll().forEach((pokemon) => getPokemonDescription(pokemon));
 document.write("<br>");
-
-//Testing
-//add correct pokemon format
-let testPokemon1 = {
-  name: "Rayquaza",
-  height: 7,
-  types: ["Dragon", "Flying"],
-};
-pokemonRepository.add(testPokemon1);
-
-//check repository
-pokemonRepository.getAll().forEach((pokemon) => getPokemonDescription(pokemon));
-document.write("<br>");
-
-//add incorrect pokemon format
-//name not string
-let testPokemon2 = {
-  name: 0,
-  height: 7,
-  types: ["Dragon", "Flying"],
-};
-pokemonRepository.add(testPokemon2);
-
-//height not number
-let testPokemon3 = {
-  name: "Rayquaza",
-  height: "7",
-  types: ["Dragon", "Flying"],
-};
-pokemonRepository.add(testPokemon3);
-
-//type 1 is not string
-let testPokemon4 = {
-  name: "Rayquaza",
-  height: 7,
-  types: [0, "Flying"],
-};
-pokemonRepository.add(testPokemon4);
-
-//type 2 is not string
-let testPokemon5 = {
-  name: "Rayquaza",
-  height: 7,
-  types: ["Dragon", 0],
-};
-pokemonRepository.add(testPokemon5);
